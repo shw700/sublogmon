c21fcf344e11bd54f6a7cc8bf2714a113abd38ef
shw <shw700@bla.com>
2016-07-22 19:42:54 +0000

    Added proper log line buffering (in case multiple entries are read, or log reading terminates mid-line). Added proper re-seek-to-EOF code. Suppressed superfluous output.

--- log.go
+++ log.go
@@ -1,6 +1,5 @@
 package main
 
-
 import "fmt"
 import "log"
 import "os"
@@ -8,8 +7,10 @@ import "regexp"
 import "strings"
 import "golang.org/x/exp/inotify"
 
+
 const ANSI_COLOR_RED =		"\033[0;31m"
 const ANSI_COLOR_RED_BOLD =	"\033[1;31m"
+const ANSI_COLOR_YELLOW =	"\033[0;33m"
 const ANSI_COLOR_RESET =	"\033[0m"
 
 
@@ -30,6 +31,7 @@ type LogAuditFile struct {
 	PathName string
 	Filters []LogFilter
 	f *os.File
+	Backlog string
 }
 
 var ( AuditLogs  = []LogAuditFile {
@@ -60,7 +62,14 @@ var ( AuditLogs  = []LogAuditFile {
 		{ Regexp: ".+kernel:.+PAX: terminating task: (?P<application>.+):[0-9]+,.+",
 			Fields: []string{ "application" },
 			OutputStr: "PAX terminated process: {application}",
-			OutputAttr: ANSI_COLOR_RED_BOLD } } } }
+			OutputAttr: ANSI_COLOR_RED_BOLD },
+		{ Regexp: ".+kernel:.+grsec: denied (?P<action>.+?) .+ by (?P<application>.+?)\\[.+",
+			Fields: []string{ "action", "application"},
+			OutputStr: "grsec denied operation {action} to application {application}",
+			OutputAttr: ANSI_COLOR_YELLOW },
+		{ Regexp: ".+kernel:.+grsec: (?P<grsecmsg>.+)",
+			Fields: []string{ "grsecmsg" },
+			OutputStr: "grsec msg: {grsecmsg}" } } } }
 )
 
 
@@ -73,8 +82,11 @@ func testRegexp(logIndex int, filterIndex int, expression string) {
 	fmt.Println("Being matched against: ", expression)
 
 	match := re.FindStringSubmatch(expression)
-	fmt.Println("match = ", match)
-	fmt.Printf("XXX: %q\n", re.SubexpNames())
+
+	if match == nil {
+		fmt.Println("There was no match.")
+		return
+	}
 
 	rmap := make(map[string]string)
 	for i, name := range re.SubexpNames() {
@@ -124,17 +136,14 @@ func main() {
 
 /*	
 	fmt.Println("Attempting test...")
-	testRegexp(1, 0, "Jul 18 17:44:52 subgraph oz-daemon[1273]: 2016/07/18 17:44:52 [ricochet] (stderr) E [FATAL] Error (exec): no such file or directory /usr/bin-oz/ricochet")
+	testRegexp(1, 0, "Jul 21 18:58:32 subgraph oz-daemon[1281]: 2016/07/21 18:58:32 [gedit] (stderr) E [FATAL] Error (exec): no such file or directory /usr/bin-oz/gedit")
 	fmt.Println("Exiting.")
-	os.Exit(0) 
+	os.Exit(0)
 */
-	
 
 	if os.Getuid() > 0 {
 		fmt.Println("Warning: this program probably won't run unless you execute it as root.")
 	}
-
-
 
 	for i := 0; i < len(AuditLogs); i++ {
 		f, err := os.OpenFile(AuditLogs[i].PathName, os.O_RDONLY, 0666)
@@ -149,9 +158,13 @@ func main() {
 			log.Fatal("Could not call stat on log file: ", err)
 		}
 
-		fmt.Printf("total log file size for %s  is %d\n", AuditLogs[i].PathName, fi.Size())
+		// fmt.Printf("total log file size for %s  is %d\n", AuditLogs[i].PathName, fi.Size())
 
 		ret, err := f.Seek(0, os.SEEK_END)
+
+		if err != nil {
+			log.Fatal("Unexpected problem occurred while attempting to skip to end of log file: ", err)
+		}
 
 		if (ret != fi.Size()) {
 			log.Fatal("Unexpected problem occurred when attempting to skip to end of log file")
@@ -173,8 +186,8 @@ func main() {
 
 	for i := 0; i < len(AuditLogs); i++ {
 		fmt.Println("Adding inotify watcher for service:", AuditLogs[i].Desc)
-//		err = watcher.Watch(AuditLogs[i].PathName)
-		err = watcher.AddWatch(AuditLogs[i].PathName, inotify.IN_MODIFY)
+//		err = watcher.AddWatch(AuditLogs[i].PathName, inotify.IN_MODIFY)
+		err = watcher.AddWatch(AuditLogs[i].PathName, inotify.IN_ALL_EVENTS)
 
 		if err != nil {
 			log.Fatal("Could not set up watcher on log file: ", err)
@@ -182,7 +195,7 @@ func main() {
 
 	}
 
-	fmt.Printf("Done now.\n")
+	fmt.Printf("Done loading, going into I/O loop.\n")
 
 	dbuf := make([]byte, BUFSIZE)
 
@@ -190,12 +203,26 @@ func main() {
 
 		select {
 			case ev := <-watcher.Event:
-//				fmt.Println("watcher event")
-//				log.Println("event: ", ev)
-//				fmt.Printf("mask was %x\n", ev.Mask)
+				// fmt.Println("watcher event")
+				// log.Println("event: ", ev)
+				// fmt.Printf("mask was %x\n", ev.Mask)
 
-				if (ev.Mask != inotify.IN_MODIFY) {
-					fmt.Println("Received unexpected notification event type... ignoring.")
+				switch (ev.Mask) {
+					case inotify.IN_ACCESS:
+						fallthrough
+					case inotify.IN_CLOSE_WRITE:
+						fallthrough
+					case inotify.IN_CLOSE_NOWRITE:
+						fallthrough
+					case inotify.IN_OPEN:
+						//fmt.Println("caught an event of no importance!")
+						continue
+				}
+
+//Important: IN_MOVE_SELF, IN_ATTRIB(delete)
+
+				if (ev.Mask & inotify.IN_MODIFY != inotify.IN_MODIFY) {
+					fmt.Printf("Received unexpected notification event type (%x)... ignoring.\n", ev.Mask)
 					continue
 				}
 
@@ -224,52 +251,72 @@ func main() {
 				}
 
 				if nread == 0 {
-//					log.Fatal("Reached end of log file... this should not have happened!")
-					fmt.Printf("Reached EOF for %s; attempting to ignore.\n", AuditLogs[i].PathName)
+					// fmt.Printf("Reached EOF for %s; attempting to ignore.\n", AuditLogs[i].PathName)
+					_, err := AuditLogs[i].f.Seek(0, os.SEEK_END)
+
+					if err != nil {
+						fmt.Println("Seek failed: ", err)
+					}
+
 					continue
 				}
 
-//				fmt.Printf("ended up reading %d bytes from log\n", nread)
+				//fmt.Printf("ended up reading %d bytes from log\n", nread)
 
 				sbuf := string(dbuf[:nread])
+				AuditLogs[i].Backlog += sbuf
 
-				for j := 0; j < len(AuditLogs[i].Filters); j++ {
-//					fmt.Printf("trying regex filter %d: %s\n", j, AuditLogs[i].Filters[j].Regexp)
-//					fmt.Printf("AGAINST: %s\n", sbuf)
+				nIndex := strings.Index(AuditLogs[i].Backlog, "\n")
 
-					match := AuditLogs[i].Filters[j].Regcomp.FindStringSubmatch(sbuf)
+				if nIndex == -1 {
+					continue
+				}
 
-					if match == nil {
-//						fmt.Printf("%sdid not match, continuing%s\n", ANSI_COLOR_RED_BOLD, ANSI_COLOR_RESET)
-						continue
-					}
+				for nIndex != -1 {
+					curLine := AuditLogs[i].Backlog[0:nIndex]
+					//fmt.Printf("current line = |%s|\n", curLine)
+					AuditLogs[i].Backlog = AuditLogs[i].Backlog[nIndex+1:]
+					nIndex = strings.Index(AuditLogs[i].Backlog, "\n")
 
-					rmap := make(map[string]string)
+					for j := 0; j < len(AuditLogs[i].Filters); j++ {
+						// fmt.Printf("trying regex filter %d: %s\n", j, AuditLogs[i].Filters[j].Regexp)
+						// fmt.Printf("AGAINST: %s\n", curLine)
 
-					for k, name := range AuditLogs[i].Filters[j].Regcomp.SubexpNames() {
+						match := AuditLogs[i].Filters[j].Regcomp.FindStringSubmatch(curLine)
 
-						if k != 0 {
-							rmap[name] = match[k]
+						if match == nil {
+//							fmt.Printf("%sdid not match, continuing%s\n", ANSI_COLOR_RED_BOLD, ANSI_COLOR_RESET)
+							continue
 						}
 
-					}
+						rmap := make(map[string]string)
 
-/*					for k := 0; k < len(AuditLogs[i].Filters[j].Fields); k++ {
-						fstr := AuditLogs[i].Filters[j].Fields[k]
-						fmt.Printf("Extracting field: %s = %s\n", fstr, rmap[fstr])
-					} */
+						for k, name := range AuditLogs[i].Filters[j].Regcomp.SubexpNames() {
 
-					outstr := formatOutput(AuditLogs[i].Filters[j].OutputStr, rmap)
+							if k != 0 {
+								rmap[name] = match[k]
+							}
 
-				        if len(outstr) == 0 {
-						fmt.Println("*** Filter condition was matched but no output string was generated")
-					} else {
-
-				                if len(AuditLogs[i].Filters[j].OutputAttr) > 0 {
-							outstr = AuditLogs[i].Filters[j].OutputAttr + outstr + ANSI_COLOR_RESET
 						}
 
-						fmt.Println("OUTPUT: ", outstr)
+						/*for k := 0; k < len(AuditLogs[i].Filters[j].Fields); k++ {
+							fstr := AuditLogs[i].Filters[j].Fields[k]
+							fmt.Printf("Extracting field: %s = %s\n", fstr, rmap[fstr])
+						} */
+
+						outstr := formatOutput(AuditLogs[i].Filters[j].OutputStr, rmap)
+
+					        if len(outstr) == 0 {
+							fmt.Println("*** Filter condition was matched but no output string was generated")
+						} else {
+
+					                if len(AuditLogs[i].Filters[j].OutputAttr) > 0 {
+								outstr = AuditLogs[i].Filters[j].OutputAttr + outstr + ANSI_COLOR_RESET
+							}
+
+							fmt.Println("* ", outstr)
+						}
+
 					}
 
 				}
